e546eb1d51150431d6202c3b7642841f8cc1ee26
kryss88ltj <christine.stauffer@gmail.com>
2014-03-19 14:07:36 -0400

    implemented facebook auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Bloccit::Application.routes.draw do
 
   get "comments/create"
 
-  devise_for :users
-
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  
   resources :topics do
     resources :posts, except: [:index] do
       resources :comments, only: [:create, :destroy]
